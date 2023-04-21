a3ca6dac4dee4b577fee42153dafd8ad57713773
Omer Hamerman <omerxx@gmail.com>
2023-04-21 17:39:24 +0100

    updating additional mappings, noice, trouble

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,13 +12,35 @@ vim.api.nvim_set_keymap("n", "tk", ":bnext<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tj", ":bprev<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "th", ":bfirst<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tl", ":blast<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "E", "$", {noremap=false})
 vim.api.nvim_set_keymap("n", "B", "^", {noremap=false})
-vim.api.nvim_create_user_command('FTermOpen', require('FTerm').open, { bang = true })
-vim.api.nvim_create_user_command('FTermClose', require('FTerm').close, { bang = true })
-vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
+vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "tf", ":TodoTelescope<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "ss", ":noh<CR>", {noremap=true})
+
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set('n', '<space><space>', "<cmd>set nohlsearch<CR>")
+
 
 vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit -m \"", {noremap=false})
 vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push -u origin HEAD<CR>", {noremap=false})
