4a43747bc956471a681a21f5625b0fb2da2ad388
Omer Hamerman <omerxx@gmail.com>
2023-04-21 17:40:24 +0100

    lualine noice

--- nvim/init.lua
+++ nvim/init.lua
@@ -319,6 +319,13 @@ require('lualine').setup {
     section_separators = '',
   },
   sections = {
+    lualine_x = {
+      {
+        require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = { fg = "#ff9e64" },
+      }
+    },
     lualine_a = {
       {
         'buffers',
