23467c61d345326411854c65a4f299f2d6695506
Omer Hamerman <omerxx@gmail.com>
2024-02-18 13:35:48 +0200

    some breaking changes after updating all

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -4,13 +4,6 @@ require('git-worktree').setup()
 -- Enable Comment.nvim
 require('Comment').setup()
 
--- Enable `lukas-reineke/indent-blankline.nvim`
--- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
-
 -- Go
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
