55e4ffc54c93d9955fe9e3a9a6a91e09fba73346
Omer Hamerman <omerxx@gmail.com>
2024-05-01 20:36:23 +0100

    adding status to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -10,7 +10,12 @@ require('lualine').setup {
         require("noice").api.statusline.mode.get,
         cond = require("noice").api.statusline.mode.has,
         color = { fg = "#ff9e64" },
-      }
+      },
+      {
+        require("noice").api.status.command.get,
+        cond = require("noice").api.status.command.has,
+        color = { fg = "#ff9e64" },
+      },
     },
     lualine_a = {
       {
