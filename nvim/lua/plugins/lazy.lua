66d745864e94e6ca35d6e6750c78f4035a1298ce
Omer Hamerman <omerxx@gmail.com>
2024-05-31 07:20:45 +0100

    sessionx and floax configs

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -61,14 +61,6 @@ require('lazy').setup({
     version = "*",  -- recommended, use latest release instead of latest commit
     lazy = true,
     ft = "markdown",
-
-    keys = {
-      { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note", mode = "n" },
-      { "<leader>oo", "<cmd>ObsidianSearch<cr>", desc = "Search Obsidian notes", mode = "n" },
-      { "<leader>os", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switch", mode = "n" },
-      { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show location list of backlinks", mode = "n" },
-      { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "n" },
-    },
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
