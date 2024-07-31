fffac07f46987fbb6b3dfef9113fae5875a1332d
Omer Hamerman <omerxx@gmail.com>
2024-07-31 14:28:30 +0100

    updating darwin flake and adding home manager

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,48 +5,40 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [ 
-	pkgs.vim
+          pkgs.vim
+          pkgs.direnv
+          pkgs.age
+          pkgs.sshs
+          pkgs.atac
+          pkgs.termshark
+          pkgs.portal
+          pkgs.glow
         ];
-
-      # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      services.spacebar.enable = true;
-      services.spacebar.package = pkgs.spacebar;
-      services.spacebar.config = {
-        clock_format     = "%R";
-        background_color = "0xff202020";
-        foreground_color = "0xffa8a8a8";
-      };
-
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
-    
       security.pam.enableSudoTouchIdAuth = true;
+
+      users.users.omerxx.home = "/Users/omerxx";
+      home-manager.backupFileExtension = "backup";
+      nix.configureBuildUsers = true;
+      nix.useDaemon = true;
 
       system.defaults = {
         dock.autohide = true;
@@ -67,15 +59,18 @@
       homebrew.brews = [
 	      "imagemagick"
       ];
-      services.redis.enable = true;
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Omers-MacBook-Pro
     darwinConfigurations."Omers-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
       modules = [ 
 	configuration
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.omerxx = import ./home.nix;
+        }
       ];
     };
 
---
+++ nix-darwin/home.nix
@@ -0,0 +1,47 @@
+# home.nix
+# home-manager switch 
+
+{ config, pkgs, ... }:
+
+{
+  home.username = "omerxx";
+  home.homeDirectory = "/Users/omerxx";
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+# Makes sense for user specific applications that shouldn't be available system-wide
+  home.packages = [
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
+    ".config/wezterm".source = ~/dotfiles/wezterm;
+    ".config/skhd".source = ~/dotfiles/skhd;
+    ".config/starship".source = ~/dotfiles/starship;
+    ".config/zellij".source = ~/dotfiles/zellij;
+    ".config/nvim".source = ~/dotfiles/nvim;
+    ".config/nix".source = ~/dotfiles/nix;
+    ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
+    ".config/tmux".source = ~/dotfiles/tmux;
+  };
+
+  home.sessionVariables = {
+  };
+
+  home.sessionPath = [
+    "/run/current-system/sw/bin"
+      "$HOME/.nix-profile/bin"
+  ];
+  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      # Add any additional configurations here
+      export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+    '';
+  };
+}
