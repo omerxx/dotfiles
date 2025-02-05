2c72c46cd33e62813eefa420a1e84ce287d024dd
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-05 20:51:23 +0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-cd ~/dotfiles
+cd ~/dotfiles       
 brew bundle dump --file=~/dotfiles/Brewfile --force
 tar -cf - github-copilot raycast | openssl enc -aes-256-cbc -pbkdf2 -e -out archive.tar.gz
 cd zed
 tar -cf - conversations prompts | openssl enc -aes-256-cbc -pbkdf2 -e -out archive.tar.gz
-cd ~/dotfiles
-rm -rf .git      
-git init           
+cd ~/dotfiles       
 git add .         
 git commit -m "My dotfiles synced from remote machines"
-git branch -M main 
-git remote add origin https://github.com/omerxx/dotfiles
-git push --force origin main
+git push origin main
 echo "✅ Complete!"
